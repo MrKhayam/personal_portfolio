c124dce3cde8cb7f711b9a7308e87b12bcb183bf
unknown <ijazkhayam@gmail.com>
2024-10-22 10:04:35 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <>
           <div className="hero w-full flex items-center relative justify-center flex-col gap-3">
               <h1 className='font-[Charley] text-white text-6xl md:text-9xl'>KHAYAM</h1>
-              <div className="w-[300px] flex items-center overflow-hidden justify-center rounded-xl mx-auto h-[400px] bg-white md:hidden">
+              <div className="w-[300px] mb-10 flex items-center overflow-hidden justify-center rounded-xl mx-auto h-[400px] bg-white md:hidden">
                   <img className='w-[100%] h-full rounded-xl' src="/dp.PNG" alt="" />
               </div>
               <Footer />
