e90a583b6234930396d183b22107bc4623a5b8cc
unknown <ijazkhayam@gmail.com>
2024-10-23 18:42:34 +0500

    update

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 const AboutHero = () => {
   return (
     <>
-     <h1 className='text-white'>About Hero Section</h1> 
+      <div className="text-white md:w-[90%] md:p-0 px-5 gap-3 w-[95%] mx-auto md:mt-[16vh] justify-center items-center md:items-start mt-24 md:gap-5 flex flex-col">
+        <h1 className='md:text-8xl text-6xl font-["Charley"]'>About Me</h1>
+        <h2 className='md:text-3xl text-center text-lg font-light md:w-[70vw]'>
+          A passionate Fullstack Web Developer, UI/UX Designer, and Graphic
+          Designer with a deep focus on delivering impactful digital
+          experiences. With my expertise in web development, I craft responsive,
+          high-performance websites that merge seamless functionality with
+          intuitive design. I take pride in translating ideas into clean code
+          and user-friendly interfaces that not only meet but exceed
+          expectations.
+        </h2>
+      </div>
     </>
-  )
+  );
 }
 
 export default AboutHero
